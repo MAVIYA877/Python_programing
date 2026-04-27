994ed9bf154c389b0e7ce8794dd9067aca26187a
MAVIYA877 <mohdmaviya425@gmail.com>
2026-04-27 21:29:30 +0530

    add code

---
+++ function.py
@@ -0,0 +1,17 @@
+#print function 
+def greet():
+    print("Hello mr.yes")
+
+greet()
+
+def name(name):
+    print("Hello",name)
+
+name("yes")
+
+#function addition 
+def add(a, b):
+    return a + b
+
+result = add(5,10)
+print(result)
